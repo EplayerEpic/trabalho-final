8e51218aaeca13e279a1a1bf3927cdf8ba2cede3
EplayerEpic <114813889+EplayerEpic@users.noreply.github.com>
2022-12-16 15:03:53 -0300

    Add files via upload

---
+++ Natsuki.js
@@ -0,0 +1,40 @@
+var Natsuki = prompt("Digite algo para continuar")
+
+if (Natsuki = "Natsuki"){
+
+alert("Ei!, Essa sou Eu!")
+
+
+}
+
+
+else if ( Natsuki == "Enzo"){
+
+    alert("Ele e legal")
+
+
+
+}
+
+else if ( Natsuki == "Sayori"){
+
+    alert("Ela e bem agitada e animada")
+
+
+
+}
+
+else if ( Natsuki == "Yuri"){
+
+    alert("Ela e bem calma e o")
+
+
+
+}
+
+else{
+
+alert("Muito bem, pode continuar!")
+
+
+}
